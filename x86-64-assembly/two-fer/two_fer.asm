5ec3160fe21983c984cb05b4ec3f4f3ff3672499
Thomas Mailund <mailund@birc.au.dk>
2020-12-01 15:48:09 +0100

    two_fer.asm

---
+++ x86-64-assembly/two-fer/two_fer.asm
@@ -0,0 +1,65 @@
+default rel
+
+section .data
+one_for_prefix: db "One for ", 0
+one_for_suffix: db ", one for me.", 0
+you:            db "you", 0
+
+section .text
+copy:
+    ; This function will copy a zero-terminated string from one
+    ; buffer to another.
+    ; Input are in rdi & rsi, we copy rsi <- rdi, we return length in rax.
+    ; We copy in this direction because it makes it easier to work with
+    ; the two_fer() function, but it is opposite the direction for
+    ; strcpy().
+    mov rax, 0
+  .copy_loop:
+    cmp byte[rdi + rax], 0 ; do we point to zero?
+    je .done               ; then we are done
+
+    ; FIXME: I don't know if I can move a byte smarter...
+    mov cl, byte[rdi + rax]
+    mov byte[rsi + rax], cl
+    inc rax
+    jmp .copy_loop
+    
+  .done:
+    ret
+
+global two_fer
+two_fer:
+    push rbp
+    mov rbp, rsp
+
+    ; save the name for later...
+    cmp rdi, 0
+    jne .save_name
+    lea rdi, [you]
+  .save_name:
+    ; Put the name on the stack so it is safe to
+    ; call a function, so we have the pointer later.
+    mov [rbp - 8], rdi
+    sub rsp, 8
+
+    ; the output buffer is in rsi, so copy into it
+    ; from rdi
+    lea rdi, [one_for_prefix]
+    call copy
+
+    ; copy the name...
+    mov rdi, [rbp - 8]
+    add rsi, rax ; continue copying from last byte...
+    call copy
+
+    ; copy the suffix
+    lea rdi, [one_for_suffix]
+    add rsi, rax ; continue copying from last byte...
+    call copy
+
+    mov byte[rsi + rax], 0 ; zero terminate
+
+    ; clean up and return...
+    add rsp, 8
+    pop rbp
+    ret
