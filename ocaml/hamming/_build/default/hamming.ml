fb0c9506ab90107ae96a238a20f62feed3309e29
Thomas Mailund <tm@kvantify.dk>
2025-02-18 19:53:15 +0100

    OCaml exercise

--- ocaml/hamming/_build/default/hamming.ml
+++ ocaml/hamming/_build/default/hamming.ml
@@ -2,9 +2,8 @@ type nucleotide = A | C | G | T
 
 let hamming_distance (xs: nucleotide list) (ys: nucleotide list) : (int, string) Result.t =
   match xs, ys with
-  | [], [] -> Ok 0
-  | [], _ -> Error "left strand must not be empty"
-  | _, [] -> Error "right strand must not be empty"
+  | [], _::_ -> Error "left strand must not be empty"
+  | _::_, [] -> Error "right strand must not be empty"
   | _, _ ->
     let rec loop xs ys acc =
       match xs, ys with
